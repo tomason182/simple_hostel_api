0756f6b5653fbf7ef9c915728085cce71e7df985
tomason182 <elotro182@hotmail.com>
2025-01-15 15:06:17 -0300

    add: configure mongodb connection

---
+++ src/adapters/config/mongo_config.js
@@ -0,0 +1,73 @@
+const logger = require("../../utils/logger");
+const { MongoClient } = require("mongodb");
+
+class MongoConnect {
+  constructor() {
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+      throw new Error("MongoDB URI is not defined");
+    }
+
+    const maxPoolSize = parseInt(process.env.MONGO_POOL_SIZE) || 100;
+    const connectTimeoutMS =
+      parseInt(process.env.MONGO_CONNECT_TIMEOUT_MS) || 30000;
+
+    this.client = new MongoClient(uri, {
+      maxPoolSize,
+      connectTimeoutMS,
+    });
+    this.isConnected = false;
+    this.retryDelay = 5000;
+  }
+
+  async connectClient(retries = 3) {
+    try {
+      if (this.isConnected) {
+        logger.info("Already connected to MongoDB");
+        return;
+      }
+
+      await this.client.connect();
+      this.isConnected = true;
+      logger.info("Connected to MongoDb");
+    } catch (e) {
+      logger.error(`MongoDB connection error: ${e.message}`);
+
+      if (retries > 0) {
+        logger.info(
+          `Retrying connection in ${this.retryDelay}ms. Attempts remaining: ${retries}`
+        );
+        await new Promise(resolve => setTimeout(resolve, this.retryDelay));
+        return this.connectClient(retries - 1);
+      } else {
+        logger.error(
+          "Max retries attempts reached. Failed to connect to MongoDB"
+        );
+        throw error;
+      }
+    }
+  }
+
+  async disconnect() {
+    try {
+      if (this.isConnected) {
+        await this.client.close();
+        this.isConnected = false;
+        logger.log("MongoDB connection close");
+      }
+    } catch (e) {
+      logger.error("Error closing connection", e);
+      throw e;
+    }
+  }
+
+  getClient() {
+    if (!this.isConnected) {
+      throw new Error("MongoDB client is not connected");
+    }
+    return this.client;
+  }
+}
+
+const connect = new MongoConnect();
+module.exports = connect;
--- src/app.js
+++ src/app.js
@@ -22,17 +22,52 @@ const {
   errorHandler,
 } = require("./middleware/errorMiddleware");
 
+// Define a stream object with a write function for morgan to use
+const stream = {
+  write: message => {
+    logger.info(message.trim());
+  },
+};
+
 const app = express();
 
-app.use(morgan("dev"));
+const corsOptions = {
+  origen:
+    process.env.NODE_ENV === "production"
+      ? ["https://simplehostel.net", "https://www.simplehostel.net"]
+      : ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(morgan("combined", { stream }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static(path.join(__dirname, "public")));
+
+// Compress all responses
+app.use(compression());
+
+// Set up headers
+app.use(helmet());
+
+// Set rate limit
+// Limit each IP to 100 request per window.
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
 
 // Use error middleware
 app.use(notFound);
 app.use(errorLog);
 app.use(errorHandler);
+
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error("Unhandled rejection", reason);
+});
 
 module.exports = app;
