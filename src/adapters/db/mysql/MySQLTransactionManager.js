d9a30f4a99f7858ace4780386ed478d33ca57448
tomason182 <elotro182@hotmail.com>
2025-01-16 13:07:18 -0300

    add: MySQL transaction manager adapter

---
+++ src/adapters/db/mysql/MySQLTransactionManager.js
@@ -0,0 +1,28 @@
+const {
+  TransactionManagerPort,
+} = require("../../../core/ports/TransactionManagerPort");
+
+export class MySQLTransactionManager extends TransactionManagerPort {
+  constructor(mysqlPool) {
+    super();
+    this.mysqlPool = mysqlPool;
+  }
+
+  async runInTransaction(operation) {
+    const conn = await this.mysqlPool.getConnection();
+    try {
+      await conn.beginTransaction();
+
+      // Execute the operation within the transaction
+      const result = await operation(conn);
+
+      await conn.commit();
+      return result;
+    } catch (e) {
+      await conn.rollback();
+      throw e;
+    } finally {
+      await conn.release(); // Release the connection back to the pool.
+    }
+  }
+}
