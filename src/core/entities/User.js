de7ba4eeadc56141c844dd53cccc102399fcd4f8
tomason182 <elotro182@hotmail.com>
2025-01-16 14:49:54 -0300

    fix: use an object in constructor

--- src/core/entities/User.js
+++ src/core/entities/User.js
@@ -1,5 +1,5 @@
 class User {
-  constructor(username, first_name, last_name) {
+  constructor({ username, first_name, last_name }) {
     this.username = username;
     this.first_name = first_name;
     this.last_name = last_name;
