6e00547cb5d7840fb613cb6d22521ea486ec1c85
tomason182 <elotro182@hotmail.com>
2025-01-17 09:23:57 -0300

    add: CreateProperty method on PropertyService

---
+++ src/core/PropertyService.js
@@ -0,0 +1,23 @@
+const Property = require("./entities/Property");
+
+class PropertyService {
+  constructor(PropertyRepository) {
+    this.PropertyRepository = PropertyRepository;
+  }
+
+  async createProperty(propertyName, email, connection = null) {
+    try {
+      const property = new Property({
+        propertyName: propertyName,
+        email: email,
+      });
+
+      await this.PropertyRepository.save(property, connection);
+      return property;
+    } catch (e) {
+      throw `An error occurred when trying to create a property: ${e.message}`;
+    }
+  }
+}
+
+module.exports = PropertyService;
