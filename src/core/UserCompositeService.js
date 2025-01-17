b7b01565c040943b3f2bd476132fcbbb9e85a0d4
tomason182 <elotro182@hotmail.com>
2025-01-17 12:59:33 -0300

    add: User Composite Service
    A class that allow handle in a single transaction the creation of a user with a property and a access control.

---
+++ src/core/UserCompositeService.js
@@ -0,0 +1,36 @@
+export class UserCompositeService {
+  constructor(
+    userService,
+    propertyService,
+    accessControlService,
+    transactionManagerPort
+  ) {
+    this.userService = userService;
+    this.propertyService = propertyService;
+    this.accessControlService = accessControlService;
+    this.transactionManagerPort = transactionManagerPort;
+  }
+
+  async createUserWithProperty(userData, PropertyData) {
+    return this.transactionManagerPort.runInTransaction(async connection => {
+      try {
+        const user = await this.userService.createUser(userData, connection);
+        const property = await this.propertyService.createProperty(
+          PropertyData,
+          connection
+        );
+        const role = "owner";
+        const accessControlID = await this.accessControlService.save(
+          user.id,
+          property.id,
+          role,
+          connection
+        );
+
+        return accessControlID;
+      } catch (e) {
+        throw e;
+      }
+    });
+  }
+}
