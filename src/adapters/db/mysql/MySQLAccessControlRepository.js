327f0281d43a122678059630c8404564ac8a4392
tomason182 <elotro182@hotmail.com>
2025-01-17 12:46:23 -0300

    add: Create accesControl repository adapter

---
+++ src/adapters/db/mysql/MySQLAccessControlRepository.js
@@ -0,0 +1,22 @@
+import { AccessControlService } from "../../../core/ports/AccessControlService";
+
+export class MySQLAccessControlRepository extends AccessControlService {
+  constructor(mysqlPool) {
+    super();
+    this.pool = mysqlPool;
+  }
+
+  async save(userId, propertyId, role, connection) {
+    try {
+      // Create access control for user with role.
+      const query =
+        "INSERT INTO access_control (user_id, property_id, role) VALUES (?,?,?)";
+      const params = [userId, propertyId, role];
+
+      const [result] = (connection || this.pool).execute(query, params);
+      return result.insertId;
+    } catch (e) {
+      throw e;
+    }
+  }
+}
