3acb6ae0abec1e43b8cb0add714c07099978b198
tomason182 <elotro182@hotmail.com>
2025-01-20 12:58:17 -0300

    add: nodemailer configuration

---
+++ src/adapters/config/nodemailerConfig.js
@@ -0,0 +1,27 @@
+import { NodemailerEmailNotificationRepository } from "../notifications/email/NodemailerEmailNotificationRepository";
+
+export function createEmailNotification() {
+  const nodemailerConfig = {
+    host:
+      process.env.NODE_ENV === "production"
+        ? "smtp.hostinger.com"
+        : "smtp.ethereal.email",
+    port: process.env.NODE_ENV === "production" ? 465 : 587,
+    secure: process.env.NODE_ENV === "production",
+    auth: {
+      user:
+        process.env.NODE_ENV === "production"
+          ? process.env.ACCOUNT_USER_PROD
+          : process.env.ACCOUNT_USER_DEV,
+      pass:
+        process.env.NODE_ENV === "production"
+          ? process.env.ACCOUNT_PASS_PROD
+          : process.env.ACCOUNT_PASS_DEV,
+    },
+    tls: {
+      rejectUnauthorized: process.env.NODE_ENV === "production",
+    },
+  };
+
+  return new NodemailerEmailNotificationRepository(nodemailerConfig);
+}
