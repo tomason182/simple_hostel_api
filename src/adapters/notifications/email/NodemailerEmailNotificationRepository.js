bb34d3b87d3eced6db48a01649b403fa58f873cd
tomason182 <elotro182@hotmail.com>
2025-01-20 12:59:11 -0300

    add: email notification repository

---
+++ src/adapters/notifications/email/NodemailerEmailNotificationRepository.js
@@ -0,0 +1,24 @@
+import nodemailer from "nodemailer";
+import { emailNotificationService } from "../../../core/ports/EmailNotificationService";
+
+export class NodemailerEmailNotificationRepository extends emailNotificationService {
+  constructor(config) {
+    super();
+    this.transporter = nodemailer.createTransport(config);
+  }
+
+  async sendEmail(to, subject, body, from) {
+    try {
+      const mailOptions = {
+        from,
+        to,
+        subject,
+        html: body,
+      };
+
+      await this.transporter.sendMail(mailOptions);
+    } catch (e) {
+      throw new Error(`Failed to send email: ${e.message}`);
+    }
+  }
+}
