2ec6878fc7352cd1cccfeae60dd66c4180150e95
tomason182 <elotro182@hotmail.com>
2025-01-21 17:28:54 -0300

    add: send email to new users

--- src/core/UserCompositeService.js
+++ src/core/UserCompositeService.js
@@ -74,6 +74,18 @@ export class UserCompositeService {
           connection
         );
 
+        const token = this.tokenService.generateToken(user.id, 900); // expiration 900 seg || 15min
+
+        const confirmationLink =
+          process.env.API_URL + "accounts/email-validation/" + token;
+
+        const to = userData.username;
+        const from = `Simple Hostel <${process.env.ACCOUNT_USER}>`;
+        const subject = "Confirm your email for SimpleHostel";
+        const body = confirmationMailBody(userData, confirmationLink);
+
+        await this.emailService.sendEmail(to, subject, body, from);
+
         return accessControlID;
       } catch (e) {
         throw e;
