f3bc7afb1153081edd0b53777c47a64909563846
tomason182 <elotro182@hotmail.com>
2025-01-28 16:04:39 -0300

    add: find user by email method

--- src/adapters/db/mysql/MySQLGuestRepository.js
+++ src/adapters/db/mysql/MySQLGuestRepository.js
@@ -1,5 +1,23 @@
 export class MySQLGuestRepository {
   constructor(mysqlPool) {
-    this.mysqlPool = mysqlPool;
+    this.pool = mysqlPool;
+  }
+
+  async findGuestByEmail(email, propertyId, conn = null) {
+    try {
+      const query =
+        "SELECT * FROM guest WHERE email = ? AND property_id = ? LIMIT 1";
+      const params = [email, propertyId];
+
+      const [result] = await (conn || this.pool).execute(query, params);
+
+      console.log("Result: ", result);
+      throw Error("stop...");
+      return result[0] || null;
+    } catch (e) {
+      throw new Error(
+        `An error occurred trying to find guest by email. Error: ${e.message}`
+      );
+    }
   }
 }
