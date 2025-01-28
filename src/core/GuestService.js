dca1e04068e895c1886ed47f295e92dc7cda029c
tomason182 <elotro182@hotmail.com>
2025-01-28 16:05:22 -0300

    add: createGuest function

--- src/core/GuestService.js
+++ src/core/GuestService.js
@@ -1,15 +1,30 @@
-import { Guest } from "./entities/Guest";
+import { Guest } from "./entities/Guest.js";
 
 export class GuestService {
   constructor(guestOutputPort) {
     this.guestOutputPort = guestOutputPort;
   }
 
-  async createGuest(propertyId, userId, guestData, conn) {
+  async createGuest(propertyId, guestData, conn = null) {
     try {
       const guest = new Guest(guestData);
       // Find if guest exists
-      const guestExist = await this.guestOutputPort.findGuestByEmail();
+      const guestExist = await this.guestOutputPort.findGuestByEmail(
+        guest.getEmail(),
+        propertyId,
+        conn
+      );
+
+      if (guestExist === null) {
+        throw new Error(`Guest with email ${guest.getEmail()} already exist`);
+      }
+
+      const result = await this.guestOutputPort.saveGuest(
+        guest,
+        propertyId,
+        conn
+      );
+      return result;
     } catch (e) {
       throw e;
     }
