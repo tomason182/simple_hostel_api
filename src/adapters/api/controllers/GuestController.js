dc6c1a9b290cde9ad4ba4798cfd0d4834e72dc06
tomason182 <elotro182@hotmail.com>
2025-01-28 20:29:28 -0300

    fix: await for the response

--- src/adapters/api/controllers/GuestController.js
+++ src/adapters/api/controllers/GuestController.js
@@ -40,4 +40,41 @@ export class GuestController {
       next(e);
     }
   };
+
+  // @desc    Update an specific guest
+  // @route   PUT /api/v1/guests/update/:id
+  // @access  Private
+  updateGuest = async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+      }
+
+      const data = matchedData(req);
+      const guestData = {
+        id: data.id,
+        first_name: data.firstName,
+        last_name: data.lastName,
+        id_number: data.idNumber || null,
+        email: data.email,
+        phone_number: data.phoneNumber || null,
+        street: data.street || null,
+        city: data.city || null,
+        country_code: data.countryCode || null,
+        postal_code: data.postalCode || null,
+      };
+
+      const propertyId = req.user.property_id;
+
+      const result = await this.guestInputPort.updateGuest(
+        guestData,
+        propertyId
+      );
+
+      return res.status(200).json(result);
+    } catch (e) {
+      next(e);
+    }
+  };
 }
