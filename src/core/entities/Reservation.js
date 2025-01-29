83be414a21229a4f2b10ce96133c5de1fd360250
tomason182 <elotro182@hotmail.com>
2025-01-29 20:14:35 -0300

    add: getter and setter to reservation entity

--- src/core/entities/Reservation.js
+++ src/core/entities/Reservation.js
@@ -78,5 +78,68 @@ export class Reservation {
     this.property_id = id;
   }
 
-  // Setter and Getter for room types
+  // Setter and Getter for booking source
+  getBookingSource() {
+    return this.booking_source;
+  }
+
+  setBookingSource(source) {
+    this.booking_source = source;
+  }
+
+  // Getter and Setter for currency.
+  getCurrency() {
+    return this.currency;
+  }
+
+  setCurrency(currency) {
+    this.currency = currency;
+  }
+
+  // Getter and Setter for reservation status
+  getReservationStatus() {
+    return this.reservation_status;
+  }
+
+  setReservationStatus(status) {
+    this.reservation_status = status;
+  }
+
+  // Getter and Setter for check-in
+  getCheckIn() {
+    return this.check_in;
+  }
+
+  setCheckIn(checkIn) {
+    const date = formatDate(checkIn); // formatDate not implemented yet.
+    this.check_in = date;
+  }
+
+  // Getter and Setter for check-out
+  getCheckOut() {
+    return this.check_out;
+  }
+
+  setCheckOut(checkOut) {
+    const date = formatDate(checkOut);
+    this.check_out = date;
+  }
+
+  // Getter and Setter for special request
+  getSpecialRequest() {
+    return this.special_request;
+  }
+
+  setSpecialRequest(text) {
+    this.special_request = text;
+  }
+
+  // Getter and Setter for created by
+  setCreateBy(userId) {
+    this.created_by = userId;
+  }
+
+  getCreatedBy() {
+    return this.created_by;
+  }
 }
