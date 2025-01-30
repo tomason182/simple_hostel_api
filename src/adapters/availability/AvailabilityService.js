acae195c9a8c04dbe6b1fcc6722fc7347f16877a
tomason182 <elotro182@hotmail.com>
2025-01-30 18:32:38 -0300

    add: create availability service class

---
+++ src/adapters/availability/AvailabilityService.js
@@ -0,0 +1,32 @@
+export class availabilityService {
+  constructor(availabilityOutPutPort) {
+    this.availabilityOutPutPort = availabilityOutPutPort;
+  }
+
+  async checkAvailability(roomTypeId, checkIn, checkOut, conn = null) {
+    // Bring the availability ranges
+    const ranges = await this.availabilityOutPutPort.getRanges(
+      roomTypeId,
+      checkIn,
+      checkOut,
+      conn
+    );
+    const roomType = await this.availabilityOutPutPort.getRoomTypeById(
+      roomTypeId,
+      conn
+    );
+
+    if (roomType === null) {
+      throw new Error("Room type ID not found");
+    }
+
+    const totalBeds = "Get the total bed for the room type object";
+
+    const reservationList =
+      await this.availabilityOutPutPort.getReservationsList(
+        roomTypeId,
+        checkIn,
+        checkOut
+      );
+  }
+}
