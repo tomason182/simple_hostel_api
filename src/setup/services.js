e388bb9d23176df336191f8a8d1f54c0654adac6
tomason182 <elotro182@hotmail.com>
2025-01-30 10:08:50 -0300

    add: Initialize Reservations service, port and adapter

--- src/setup/services.js
+++ src/setup/services.js
@@ -6,9 +6,12 @@ import { MySQLUserRepository } from "../adapters/db/mysql/MySQLUserRepository.js
 import { MySQLPropertyRepository } from "../adapters/db/mysql/MySQLPropertyRepository.js";
 import { MySQLAccessControlRepository } from "../adapters/db/mysql/MySQLAccessControlRepository.js";
 import { MySQLGuestRepository } from "../adapters/db/mysql/MySQLGuestRepository.js";
+import { MySQLReservationRepository } from "../adapters/db/mysql/MySQLReservationRepository.js";
 
 // SERVICES
+import { GuestService } from "../core/GuestService.js";
 import { PropertyService } from "../core/PropertyService.js";
+import { ReservationService } from "../core/ReservationService.js";
 import { UserService } from "../core/UserService.js";
 import { UserCompositeService } from "../core/UserCompositeService.js";
 
@@ -19,6 +22,8 @@ import { PropertyInputPort } from "../core/ports/PropertyInputPort.js";
 import { PropertyOutputPort } from "../core/ports/PropertyOutputPort.js";
 import { GuestInputPort } from "../core/ports/GuestInputPort.js";
 import { GuestOutputPort } from "../core/ports/GuestOutputPort.js";
+import { ReservationInputPort } from "../core/ports/ReservationInputPort.js";
+import { ReservationOutputPort } from "../core/ports/ReservationOutputPort.js";
 import { UserTransactionManagerPort } from "../core/ports/UserTransactionManagerPort.js";
 
 // Import nodemailer email notification service
@@ -29,7 +34,7 @@ import { createTokenService } from "../adapters/config/tokenConfig.js";
 import { UserController } from "../adapters/api/controllers/UserController.js";
 import { PropertyController } from "../adapters/api/controllers/PropertyController.js";
 import { GuestController } from "../adapters/api/controllers/GuestController.js";
-import { GuestService } from "../core/GuestService.js";
+import { ReservationController } from "../adapters/api/controllers/ReservationController.js";
 
 export default function initializeServices() {
   const mysqlPool = mysqlConnect.getPool();
@@ -39,6 +44,7 @@ export default function initializeServices() {
   const propertyRepository = new MySQLPropertyRepository(mysqlPool);
   const accessControlService = new MySQLAccessControlRepository(mysqlPool);
   const guestRepository = new MySQLGuestRepository(mysqlPool);
+  const reservationRepository = new MySQLReservationRepository(mysqlPool);
 
   // INITIALIZE EXTRA SERVICES.
   const emailService = createEmailNotification();
@@ -51,17 +57,20 @@ export default function initializeServices() {
     tokenService,
     emailService
   );
-
   const propertyOutputPort = new PropertyOutputPort(propertyRepository);
   const guestOutputPort = new GuestOutputPort(
     guestRepository,
     propertyRepository
+  );
+  const reservationOutputPort = new ReservationOutputPort(
+    reservationRepository
   );
 
   // Initialize the core services
   const propertyService = new PropertyService(propertyOutputPort, mysqlPool);
   const userService = new UserService(userOutputPort);
   const guestService = new GuestService(guestOutputPort);
+  const reservationService = new ReservationService(reservationOutputPort);
 
   // Initialize transaction manager ports
   const userTransactionManagerPort = new UserTransactionManagerPort(
@@ -89,14 +98,18 @@ export default function initializeServices() {
   );
   const propertyInputPort = new PropertyInputPort(propertyService);
   const guestInputPort = new GuestInputPort(guestService);
+  const reservationInputPort = new ReservationInputPort(reservationService);
 
+  // Initialize controllers
   const userController = new UserController(userInputPort);
   const propertyController = new PropertyController(propertyInputPort);
   const guestController = new GuestController(guestInputPort);
+  const reservationController = new ReservationController(reservationInputPort);
 
   return {
     userController,
     propertyController,
     guestController,
+    reservationController,
   };
 }
