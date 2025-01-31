302a3f58d1302b94b801f512f0cc106eaedf6228
tomason182 <elotro182@hotmail.com>
2025-01-31 15:40:17 -0300

    add: rates and availabilities create rouest

---
+++ src/adapters/api/routes/ratesAndAvailabilityRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { checkSchema } from "express-validator";
+import { createTokenService } from "../../config/tokenConfig.js";
+import authMiddleware from "../../../middleware/authMiddleware.js";
+import { ratesAndAvailabilitySchema } from "../schemas/ratesAndAvailabilitySchema.js";
+
+export function createRatesAndAvailabilityRoutes(service) {
+  const router = express.Router();
+  const tokenService = createTokenService();
+
+  const rateAndAvailabilityController = service.ratesAndAvailabilityController;
+
+  // @desc Add a new rate and availability range
+  // @route POST /api/v1/rates_availability/create/:id
+  // @access Private
+  router.post(
+    "/create",
+    authMiddleware(tokenService),
+    checkSchema(ratesAndAvailabilitySchema),
+    rateAndAvailabilityController.createRange
+  );
+}
