71cde4572fb7c977afc62a2b1c107a17e22580a5
tomason182 <elotro182@hotmail.com>
2025-02-21 18:04:45 -0300

    fix: conn and pool from roomType service

--- src/core/RoomTypeService.js
+++ src/core/RoomTypeService.js
@@ -1,32 +1,31 @@
 import { RoomType } from "./entities/RoomType.js";
 
 export class RoomTypeService {
-  constructor(roomTypeOutputPort, mysqlPool) {
+  constructor(roomTypeOutputPort) {
     this.roomTypeOutputPort = roomTypeOutputPort;
-    this.pool = mysqlPool;
   }
 
-
-  async createRoomType(propertyId, roomTypeData, connection) {
+  async createRoomType(propertyId, roomTypeData) {
     try {
-
-      const roomTypeExist = await this.roomTypeOutputPort.findOneRoomTypeByDescription(
-        roomTypeData.description,
-        propertyId,
-        connection,
-      );
+      const roomTypeExist =
+        await this.roomTypeOutputPort.findRoomTypeByDescription(
+          roomTypeData.description,
+          propertyId,
+          connection
+        );
 
       if (roomTypeExist !== null) {
         throw new Error("Room Type name already exist");
       }
 
       // Limit roomType creation
-      const roomTypeList = await this.roomTypeOutputPort.findAllRoomTypesByPropertyId(
-        propertyId,
-        connection,
-      );
-      
-      const roomType = new RoomType(roomTypeData);            // esto se desestructura adentro ?
+      const roomTypeList =
+        await this.roomTypeOutputPort.findAllRoomTypesByPropertyId(
+          propertyId,
+          connection
+        );
+
+      const roomType = new RoomType(roomTypeData);
 
       roomTypeList.push(roomType);
 
@@ -43,23 +42,28 @@ export class RoomTypeService {
       }
 
       roomType.setPropertyId(propertyId);
-      // roomType.setProducts();                  ##########    AQUI SE SETEABAN LOS PRODUCTS PERO AHORA ESTOS ESTAN EN UNA TABLA APARTE
-      // RESOLVER LUEGO.
+      roomType.setProducts();
 
+      // Add room type to room type table and get back the ID.
       const result = await this.roomTypeOutputPort.save(roomType, connection);
+
+      // Add the room type products in the products table.
+      const productResult = await this.roomTypeOutputPort.saveProduct(
+        roomType.getProducts(),
+        connection
+      );
+
       return result;
     } catch (e) {
       throw e;
     }
   }
 
-  
   async readRoomTypes(propertyId) {
     try {
       // Brings all room types from a specific property
-      const roomTypesByProperty = await this.roomTypeOutputPort.findAllRoomTypesByPropertyId(
-        propertyId
-      );
+      const roomTypesByProperty =
+        await this.roomTypeOutputPort.findAllRoomTypesByPropertyId(propertyId);
 
       if (roomTypesByProperty === null) {
         throw new Error("No room types found that belong to this property");
@@ -71,10 +75,8 @@ export class RoomTypeService {
     }
   }
 
-
   async findRoomTypeById(id) {
     try {
-      
       const roomType = await this.roomTypeOutputPort.findRoomTypeById(id);
 
       if (roomType === null) {
@@ -87,46 +89,48 @@ export class RoomTypeService {
     }
   }
 
-
   async updateRoomTypeById(roomTypeData, propertyId) {
     try {
       // Check if new description is unique
       // Check if room type exist in the database
-      const roomTypeExist = await this.roomTypeOutputPort.findOneRoomTypeByDescription(
-        roomTypeData.description,
-        propertyId, 
-      );
+      const roomTypeExist =
+        await this.roomTypeOutputPort.findOneRoomTypeByDescription(
+          roomTypeData.description,
+          propertyId
+        );
 
       // If room type exist in the db, throw an error
       if (roomTypeExist !== null && !roomTypeData.id.equals(roomTypeExist.id)) {
         throw new Error("Room Type name already exist");
       }
 
-      const result = await this.roomTypeOutputPort.updateRoomTypeById(roomTypeData);
+      const result = await this.roomTypeOutputPort.updateRoomTypeById(
+        roomTypeData
+      );
 
       if (result === 0 || result === null) {
         throw new Error("Room type update could not be done");
-      }  
-      
+      }
+
       return result;
     } catch (e) {
       throw e;
     }
   }
 
-
   async deleteRoomTypeById(id) {
     try {
-
-      const roomTypeDeleted = await this.roomTypeOutputPort.deleteRoomTypeById(id);
+      const roomTypeDeleted = await this.roomTypeOutputPort.deleteRoomTypeById(
+        id
+      );
 
       if (roomTypeDeleted === null) {
         throw new Error("No room types found that belong to this ID");
       }
-      
+
       return roomTypeDeleted;
     } catch (e) {
       throw e;
     }
-  } 
+  }
 }
--- src/core/ports/RoomTypeOutputPort.js
+++ src/core/ports/RoomTypeOutputPort.js
@@ -7,34 +7,30 @@ export class RoomTypeOutputPort {
     return this.roomTypeRepository.save(roomType, connection);
   }
 
-  findOneRoomTypeByDescription(description, propertyId, conn = null) {
-    return this.roomTypeRepository.findOneRoomTypeByDescription(description, propertyId, conn);
+  findRoomTypeByDescription(description, propertyId, conn = null) {
+    return this.roomTypeRepository.findOneRoomTypeByDescription(
+      description,
+      propertyId,
+      conn
+    );
   }
 
   findAllRoomTypesByPropertyId(propertyId, conn = null) {
-    return this.roomTypeRepository.findAllRoomTypesByPropertyId(propertyId, conn);
+    return this.roomTypeRepository.findAllRoomTypesByPropertyId(
+      propertyId,
+      conn
+    );
   }
 
   findRoomTypeById(id, conn = null) {
     return this.roomTypeRepository.findRoomTypeById(id, conn);
   }
-  
+
   updateRoomTypeById(roomTypeData, conn = null) {
     return this.roomTypeRepository.updateRoomTypeById(roomTypeData, conn);
-  } 
+  }
 
   deleteRoomTypeById(id, conn = null) {
     return this.roomTypeRepository.deleteRoomTypeById(id, conn);
-  } 
-
+  }
 }
-
-
-/* ###############################          ESTO MAS QUE SEGURO NO ESTA BIEN                        ###########################################
-  ################################        ES QUE NO ME QUEDO CLARO EL CONCEPTO DE LA                 ##########################################
-  ################################        CONEXION, CUANDO SI LA REQUIERE Y CUANDO NO.                #########################################
-  ################################       PREFIERO PASARTE YA LO QUE TENGO Y QUE VOS VAYAS              ########################################
-  ################################       METIENDO MANO. YO POR MI PARTE VOY A TERMINAR DE               #######################################
-  ################################      CODIFICAR EL ULTIMO ARCHIVO QUE ME QUEDA, QUE ES EL              #######################################
-  ################################      REPOSITORY MSQL Y CUANDO LO TENGA REPASO TODO PARA IR             #######################################  
-  ################################      HACIENDO LOS AJUSTES NECESARIOS Y QUE DE UNA BUENA VEZ FUNCIONE    ####################################### */
