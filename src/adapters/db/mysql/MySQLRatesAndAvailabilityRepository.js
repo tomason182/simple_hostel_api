8568f57af2becbcd1a30264e6bc79a040f83a2e4
tomason182 <elotro182@hotmail.com>
2025-02-22 16:26:25 -0300

    add: get all property ranges

--- src/adapters/db/mysql/MySQLRatesAndAvailabilityRepository.js
+++ src/adapters/db/mysql/MySQLRatesAndAvailabilityRepository.js
@@ -12,4 +12,20 @@ export class MySQLRatesAndAvailabilityRepository {
       );
     }
   }
+
+  async getAllRanges(propertyId, conn = null) {
+    try {
+      const query =
+        "SELECT * FROM rates_and_availability WHERE property_id = ?";
+      const params = [propertyId];
+
+      const [result] = (conn || this.pool).execute(query, params);
+
+      return result || [];
+    } catch (e) {
+      throw new Error(
+        `An error occurred trying to get all property rates ranges. Error: ${e.message}`
+      );
+    }
+  }
 }
