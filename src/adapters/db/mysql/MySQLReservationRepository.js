f6dae9bba5bd75a9e88f090d373826de473d3e77
tomason182 <elotro182@hotmail.com>
2025-02-24 14:44:13 -0300

    fix: create reservation method
    Create a reservation implies adding the data in two tables reservation table and reservation_rooms table. This allow handling multiples rooms per reservation

--- src/adapters/db/mysql/MySQLReservationRepository.js
+++ src/adapters/db/mysql/MySQLReservationRepository.js
@@ -6,22 +6,37 @@ export class MySQLReservationRepository {
   async createReservation(reservation, conn = null) {
     try {
       const query =
-        "INSERT INTO reservations (guest_id, property_id, booking_source, currencies, reservation_status, check_in, check_out, special_request, created_by) VALUES(?,?,?,?,?,?,?,?,?)";
+        "INSERT INTO reservations (guest_id, property_id, booking_source, currencies, reservation_status, payment_status, check_in, check_out, special_request, created_by) VALUES(?,?,?,?,?,?,?,?,?)";
       const params = [
         reservation.getGuestId(),
         reservation.getPropertyId(),
         reservation.getBookingSource(),
         reservation.getCurrency(),
         reservation.getReservationStatus(),
+        reservation.getPaymentStatus(),
         reservation.getCheckIn(),
         reservation.getCheckOut(),
         reservation.getSpecialRequest(),
         reservation.getCreateBy(),
       ];
 
-      const [result] = (conn || this.pool).execute(query, params);
+      const [result] = await conn.execute(query, params);
 
       reservation.setId(result.insertId);
+
+      const setRoomsQuery =
+        "INSERT INTO reservation_rooms (reservation_id, room_type_id, number_of_guest, total_amount) VALUES (?,?,?,?)";
+      for (room of reservation.getSelectedRooms()) {
+        const roomParams = [
+          reservation.getId(),
+          room.room_type_id,
+          room.number_of_guest,
+          room.total_amount,
+        ];
+
+        await conn.execute(setRoomsQuery, roomParams);
+      }
+
       return reservation;
     } catch (e) {
       throw new Error(
@@ -29,18 +44,19 @@ export class MySQLReservationRepository {
       );
     }
   }
+
   async getReservationListByDateRange(
     roomTypeId,
-    checkIn,
-    checkOut,
+    startDate,
+    endDate,
     conn = null
   ) {
     try {
       const query =
-        "SELECT id, check_in, check_out, number_of_guest FROM reservations WHERE room_type_id = ? AND reservation_status NOT IN ('canceled', 'no_show') AND check_in < ? AND check_out > ?";
-      const params = [roomTypeId, checkOut, checkIn];
+        "SELECT id, check_in, check_out, number_of_guest FROM reservations WHERE room_type_id = ? AND reservation_status NOT IN ('canceled', 'no_show') AND check_in <= ? AND check_out > ?";
+      const params = [roomTypeId, endDate, startDate];
 
-      const [result] = (conn || this.pool).execute(query, params);
+      const [result] = await (conn || this.mysqlPool).execute(query, params);
 
       return result;
     } catch (e) {
