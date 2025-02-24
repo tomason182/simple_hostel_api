8bf341cd6785912c28df32f8837d289770b1ccf3
tomason182 <elotro182@hotmail.com>
2025-02-24 14:15:11 -0300

    add: create Reservation method

--- src/core/ReservationService.js
+++ src/core/ReservationService.js
@@ -7,9 +7,37 @@ export class ReservationService {
 
   async createReservation(reservationData, conn = null) {
     try {
-      console.log(reservationData);
+      const reservation = new Reservation(reservationData);
 
-      return "testing Reservation service";
+      // Check availability.
+      const selectedRooms = reservation.getSelectedRooms();
+      const checkIn = reservation.getCheckIn();
+      const checkOut = reservation.getCheckOut();
+
+      let isAvailable = true;
+      for (room in selectedRooms) {
+        roomTypeId = room.room_type_id;
+        numberOfGuest = room.number_of_guest;
+        isAvailable = await this.reservationOutport.checkAvailability(
+          roomTypeId,
+          checkIn,
+          checkOut,
+          conn
+        );
+        if (isAvailable === false) {
+          throw new Error(
+            "Unable to create reservation. No availability for the selected rooms"
+          );
+        }
+      }
+
+      // Create reservation
+      const result = await this.reservationOutport.saveReservation(reservation);
+
+      // send email to guest
+      await this.reservationOutport.sendEmail();
+
+      return result;
     } catch (e) {
       throw e;
     }
