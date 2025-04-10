1e9e96c89fc0032ca2ed6f16c507d21b2a96212f
tomason182 <elotro182@hotmail.com>
2025-04-10 11:45:08 -0300

    add: create reservation route

--- src/adapters/api/routes/bookEngineRoutes.js
+++ src/adapters/api/routes/bookEngineRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { checkSchema, param } from "express-validator";
-import { createTokenService } from "../../config/tokenConfig.js";
+import { reservationSchema } from "../schemas/reservationSchema.js";
 
 export function createBookEngineRoutes(service) {
   const router = express.Router();
@@ -40,5 +40,32 @@ export function createBookEngineRoutes(service) {
         return new Date(`${year}-${month}-${day}`);
       }),
     bookEngineController.checkAvailability
+  );
+
+  // @desc get property data
+  // @route GET /api/v2/book-engine/property/:propertyId
+  // @access Public
+  router.get(
+    "/property/:propertyId",
+    param("propertyId")
+      .trim()
+      .isInt()
+      .notEmpty()
+      .withMessage("Invalid property ID"),
+    bookEngineController.getPropertyData
+  );
+
+  // @desc Create reservation
+  // @route POST /api/v2/book-engine/reservation/:propertyId
+  // @access Public
+  router.post(
+    "/booking/:propertyId",
+    param("propertyId")
+      .trim()
+      .isInt()
+      .notEmpty()
+      .withMessage("Invalid property ID"),
+    checkSchema(reservationSchema, ["body"]),
+    bookEngineController.createReservation
   );
 }
