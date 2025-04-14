98d74efbeefde07d4683772016eb4d7b999889c9
tomason182 <elotro182@hotmail.com>
2025-04-14 10:19:41 -0300

    fix: allow localhost in cors config

--- src/app.js
+++ src/app.js
@@ -46,8 +46,8 @@ export async function createApp(services) {
     origin:
       process.env.NODE_ENV === "production"
         ? ["https://simplehostel.net", "https://www.simplehostel.net"]
-        : "*",
-    credentials: process.env.NODE_ENV === "production",
+        : ["http://localhost:39715", "http://localhost:5173"],
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   };
 
